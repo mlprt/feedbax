b10c062444bb768d2333c05b71b4358e62746b8e
Matt L Laporte <matt.l.laporte@gmail.com>
2023-11-26 11:22:54 -0500

    Refactor model substate updating into `AbstractModel`

--- feedbax/model.py
+++ feedbax/model.py
@@ -14,10 +14,12 @@ TODO:
 from abc import abstractmethod, abstractproperty
 from collections import OrderedDict
 import copy
+from functools import cached_property
 import logging
 from typing import Callable, Dict, Generic, Optional, Sequence, Tuple, TypeVar, Union
 
 import equinox as eqx
+from equinox import AbstractVar
 import jax
 import jax.numpy as jnp
 import jax.random as jr
@@ -47,8 +49,23 @@ StateT = TypeVar("StateT", bound=AbstractModelState)
 
 
 class AbstractModel(eqx.Module, Generic[StateT]):
-    """
+    """Base class for compositional state-dependent models.
+    
+    To define a new model, the following should be implemented:
+    
+        1. A concrete subclass of `AbstractModelState` that defines the PyTree
+           structure of the full model state. The fields may be types of 
+           `AbstractModelState` as well, in the case of nested models.
+        2. A concrete subclass of this class (`AbstractModel`) with:
+            i. fields to hold the modules that compose the model, 
+            ii. a `model_spec` property that specifies a series of operations 
+            on parts of the full model state, using those component modules, 
+            and
+            iii. an `init` method that returns an initial model state
+    
     TODO:
+    - Should `init` be in `AbstractModelState` rather than an `AbstractModel`?
+    
     - It might make sense to implement `__call__` here, and then get subclasses
       to implement another method (`step`?). 
       - For example, this would allow all the `intervenors` to be called at the 
@@ -56,28 +73,110 @@ class AbstractModel(eqx.Module, Generic[StateT]):
         dealing with; we'd need an `intervenors: AbstractVar` field in that 
         case. However, I'm not sure yet that this makes sense.
     """
-        
-    @abstractmethod
+    intervenors: AbstractVar[Dict[str, Sequence[AbstractIntervenor]]]
+    
+    @jax.named_scope("fbx.AbstractModel")
     def __call__(
         self, 
-        input, 
+        input, # AbstractTaskInput 
         state: StateT, 
         key: jax.Array,
     ) -> StateT:
-        ...
         
-    # @abstractmethod
-    # def init(
-    #     self,
-    #     state, 
-    # ) -> State:
-    #     ...
+        keys = jr.split(key, len(self._stages))
+        
+        for (intervenors, module, where, module_input), key \
+            in zip(self._stages, keys):
+            
+            key1, key2 = jr.split(key)
+            
+            for intervenor in intervenors:
+                # whether this modifies the state depends on `input`
+                state = intervenor(state, input, key=key1)
+                
+            state = eqx.tree_at(
+                where, 
+                state, 
+                module(self)(
+                    module_input(input, state), 
+                    where(state), 
+                    key=key2,
+                ),
+            )
+        
+        return state
+
+    @abstractproperty
+    def stages_spec(self) -> Dict[str, Tuple[
+        eqx.Module,
+        Callable[[StateT], PyTree],
+        Sequence[Callable[[AbstractTaskInput, StateT], PyTree]],
+    ]]:
+        """Specifies the model in terms of substate operations.
+        
+        Each tuple in the dict has the following elements:
+        
+            1. An equinox module that performs a state update
+            2. A function that selects the part of the model state PyTree that 
+               is updated (i.e. output) by the module
+            3. A function that selects the part of the model state PyTree that 
+               is passed as input to the module
+        """
+        ...
+  
+    @cached_property 
+    def _stages(self) -> Tuple[Tuple[
+        Sequence[AbstractIntervenor],
+        eqx.Module,
+        Callable[[StateT], PyTree],
+        Sequence[Callable[[AbstractTaskInput, StateT], PyTree]],
+    ]]: 
+        """Zips up the user-defined intervenors with `stages_spec`."""
+        #! should not be referred to in `__init__`, at least before defining `intervenors`
+        return tuple(jax.tree_map(
+            lambda x, y: (y,) + x, 
+            self.stages_spec, 
+            jax.tree_map(tuple, self.intervenors, 
+                         is_leaf=lambda x: isinstance(x, list)),
+            is_leaf=lambda x: isinstance(x, tuple),
+        ).values())
+    
+    def _get_intervenors_dict(
+        self, intervenors: Optional[Union[Sequence[AbstractIntervenor],
+                                          Dict[str, Sequence[AbstractIntervenor]]]]
+    ):
+        intervenors_dict = jax.tree_map(
+            lambda _: [], 
+            self.stages_spec,
+            is_leaf=lambda x: isinstance(x, tuple),
+        )
+        
+        if intervenors is not None:
+            if isinstance(intervenors, Sequence):
+                # By default, place interventions in the first stage.
+                intervenors_dict.update({'get_feedback': list(intervenors)})
+            elif isinstance(intervenors, dict):
+                intervenors_dict.update(
+                    jax.tree_map(list, intervenors, 
+                                 is_leaf=lambda x: isinstance(x, Sequence)))
+            else:
+                raise ValueError("intervenors not a sequence or dict of sequences")
+        
+        return intervenors_dict
+        
+    @abstractmethod
+    def init(
+        self,
+        **kwargs,
+    ) -> StateT:
+        """Return an initial state for the model."""
+        ...
         
     @abstractproperty
     def memory_spec(self) -> PyTree[bool]:
         """Specifies which states should typically be remembered by callers."""
         ...
-
+        
 
 class SimpleFeedbackState(AbstractModelState):
     mechanics: MechanicsState
@@ -103,18 +202,7 @@ class SimpleFeedback(AbstractModel[SimpleFeedbackState]):
     delay: int 
     feedback_leaves_func: Callable[[PyTree], PyTree]  
     
-    # stage_spec: Dict[str, Tuple[
-    #     Callable[[SimpleFeedbackState], PyTree],
-    #     eqx.Module,
-    #     Sequence[Callable[[AbstractTaskInput, SimpleFeedbackState], PyTree]],
-    # ]]
     intervenors: Dict[str, Sequence[AbstractIntervenor]]
-    stages: Tuple[Tuple[
-        Sequence[AbstractIntervenor],
-        Callable[[SimpleFeedbackState], PyTree],
-        eqx.Module,
-        Sequence[Callable[[AbstractTaskInput, SimpleFeedbackState], PyTree]],
-    ]]
     
     def __init__(
         self, 
@@ -122,10 +210,11 @@ class SimpleFeedback(AbstractModel[SimpleFeedbackState]):
         mechanics: Mechanics, 
         net_readout: Optional[eqx.Module] = None,
         delay: int = 0, 
-        feedback_leaves_func: Callable[[MechanicsState], PyTree] = \
-            lambda mechanics_state: mechanics_state.system,
+        feedback_leaves_func: Callable[[MechanicsState], PyTree] \
+            = lambda mechanics_state: mechanics_state.system,
         intervenors: Optional[Union[Sequence[AbstractIntervenor],
-                              Dict[str, Sequence[AbstractIntervenor]]]] = None,
+                                    Dict[str, Sequence[AbstractIntervenor]]]] \
+            = None,
         key: Optional[jax.Array] = None,
     ):
         if net_readout is None:
@@ -139,37 +228,10 @@ class SimpleFeedback(AbstractModel[SimpleFeedbackState]):
         self.net = net
         self.net_readout = net_readout
         self.mechanics = mechanics
-        self.delay = delay + 1  # indexing: delay=0 -> storage len=1, idx=-1
-        self.feedback_channel = Channel(delay)
         self.feedback_leaves_func = feedback_leaves_func
-        
-        intervention_dict = jax.tree_map(
-            lambda _: [], 
-            self.stages_spec,
-            is_leaf=lambda x: isinstance(x, tuple),
-        )
-        
-        if intervenors is not None:
-            if isinstance(intervenors, Sequence):
-                # By default, place interventions in the first stage.
-                intervention_dict.update({'get_feedback': list(intervenors)})
-            elif isinstance(intervenors, dict):
-                intervention_dict.update(
-                    jax.tree_map(list, intervenors, 
-                                 is_leaf=lambda x: isinstance(x, Sequence)))
-            else:
-                raise ValueError("intervenors not a sequence or dict of sequences")
-        
-        self.intervenors = intervention_dict
-
-        # To construct actual stages, join intervenors with stages_spec.
-        self.stages = tuple(jax.tree_map(
-            lambda x, y: (y,) + x, 
-            self.stages_spec, 
-            jax.tree_map(tuple, self.intervenors, 
-                         is_leaf=lambda x: isinstance(x, list)),
-            is_leaf=lambda x: isinstance(x, tuple),
-        ).values())
+        self.delay = delay + 1  # indexing: delay=0 -> storage len=1
+        self.feedback_channel = Channel(delay)
+        self.intervenors = self._get_intervenors_dict(intervenors)
         
     @property
     def stages_spec(self):
@@ -181,58 +243,26 @@ class SimpleFeedback(AbstractModel[SimpleFeedbackState]):
         """
         return OrderedDict({
             'get_feedback': (
-                lambda state: state.feedback,  # substate it operates on
                 lambda self: self.feedback_channel,  # module that operates on it
+                lambda state: state.feedback,  # substate it operates on
                 lambda _, state: self.feedback_leaves_func(state.mechanics),  # inputs
             ),
             'nn_step': (
-                lambda state: state.network,
                 lambda self: self.net,
+                lambda state: state.network,
                 lambda input, state: (input, state.feedback.output),
             ),
             'nn_readout': (
-                lambda state: state.net_readout,
                 lambda self: self.net_readout,
+                lambda state: state.net_readout,
                 lambda _, state: state.network.activity,
             ),
             'mechanics_step': (
-                lambda state: state.mechanics,
                 lambda self: self.mechanics,
+                lambda state: state.mechanics,
                 lambda _, state: state.net_readout,
             ),
-        })
-    
-    @jax.named_scope("fbx.SimpleFeedback")
-    def __call__(
-        self, 
-        input,  # AbstractTaskInput 
-        state: SimpleFeedbackState, 
-        key: jax.Array,
-    ) -> SimpleFeedbackState:
-        
-        keys = jr.split(key, len(self.stages))
-        
-        for (intervenors, where, module, module_input), key \
-            in zip(self.stages, keys):
-            
-            key1, key2 = jr.split(key)
-            
-            for intervenor in intervenors:
-                # whether this modifies the state depends on `input`
-                state = intervenor(state, input, key=key1)
-                
-            state = eqx.tree_at(
-                where, 
-                state, 
-                module(self)(
-                    module_input(input, state), 
-                    where(state), 
-                    key=key2,
-                ),
-            )
-        
-        return state
-        
+        })        
 
     def init(
         self, 
@@ -276,6 +306,10 @@ class SimpleFeedback(AbstractModel[SimpleFeedbackState]):
         feedback delay is 5 steps, `Iterator` would otherwise end up with 5 
         extra copies of all the parts of `states.mechanics` that are part of 
         the feedback.
+        
+        In particular, this information will be used by `Iterator`, but will
+        be ignored by `SimpleIterator`, which remembers the full state 
+        indiscriminately to favour speed over memory usage.
         
         NOTE: It makes sense for this to be here since it has to do with the
         logic of the feedback loop, i.e. that queue is just transient internal 
